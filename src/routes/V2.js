36ff5de9fca480fb8609ba21b2fd4d00332924ad
chayoung-choi <chayoung.dev@gmail.com>
2021-06-21 17:39:17 +0900

    init project

---
+++ src/routes/V2.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+class V1 extends React.Component {
+    render() {
+        const {} = this.state;
+        return (
+            <>V1</>
+        );
+    }
+}
+export default V1;
